079a1d0e37d359d1e714f29eb2e3eb70b8795b00
Vlad Khomenko <vladkhomenko@MacBook-Air-Vlad.local>
2023-02-09 12:51:54 +0200

    add header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-
+import Logo from './Logo'
 const navigation = [
     {
         id: 1,
@@ -8,38 +8,51 @@ const navigation = [
     },
     {
         id: 2,
-        title: 'Masters',
+        title: 'Products',
         path: '/master'
     },
     {
         id: 3,
-        title: 'Blog',
+        title: 'Resources',
         path: '/blog'
     },
     {
         id: 4,
-        title: 'About',
+        title: 'Pricing',
         path: '/about'
     }
 ]
 
 const Header = () => (
     <div className='header'>
-        <div className='container'>
-            <div className='header__image'>
+        <div className='container header__container'>
+            
+            <div className="header__info">
+                <div className="header__info-item">
 
+                        <Logo />
+
+                    <h2>
+                        Untitled UI
+                    </h2>
+                </div>
+                <nav className='nav'>
+                    <ul className='nav__menu'>
+                        {navigation.map(({id, title, path}) => (
+                            <li className='nav__menu-item' key={id}><Link href={path}>{title}</Link></li>
+                        ))}
+                    </ul>
+                </nav>
             </div>
-            <nav className='nav'>
-                <ul>
-                    {navigation.map(({id, title, path}) => (
-                        <li key={id}><Link href={path}>{title}</Link></li>
-                    ))}
-                </ul>
-            </nav>
+
+            
+            <div className="header__button">
+                <button>Sign Up</button>
+            </div>
+            
         </div>
-        <h2>
-            header
-        </h2>
+        <hr />
+    
     </div>
 );
 
