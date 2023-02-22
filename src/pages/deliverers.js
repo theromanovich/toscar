f892ee5521a59da9847abb8b89c0e58b758dac64
Vlad Khomenko <vladkhomenko@MacBook-Air-Vlad.local>
2023-02-22 21:33:35 +0200

    filter refactoring

--- src/pages/deliverers.js
+++ src/pages/deliverers.js
@@ -1,21 +1,33 @@
 import Filter from "@/components/Filter/Filter";
 import DelivererCard from "@/components/DelivererCard/DelivererCard";
+import { useEffect, useState } from "react";
 
 
 const deliverers = (props) => {
-  
+  const [deliverersArr, setDeliverersArr] = useState([])
   const deliverersData = props.data.data;
-  console.log(deliverersData)
-  
+
+  useEffect(() => {
+    setDeliverersArr(deliverersData)
+    console.log(deliverersArr)
+  }, [deliverersData])
+
+  // const deliverersHandler = () => {
+  //   setDeliverersArr(deliverersArr.filter(card => {
+  //     console.log(card[0])
+  //   } ))
+  // }
+
   return (
     <>
         <Filter/>
-        {deliverersData.map(deliverer => {
+        {deliverersArr.map(deliverer => {
           const {url} = deliverer.attributes.thumbnail.data[0].attributes;
           const {tags} = deliverer.attributes
           return <DelivererCard {...deliverer.attributes} thumbnail={url} tags={tags.split(', ')}/>
+          
         })}
-        
+       
     </>
   )
 }
