28475dd0984abcd5e5bf17a5fd324d9bcaf94e70
Vlad Khomenko <vladkhomenko@MacBook-Air-Vlad.local>
2023-02-22 14:08:09 +0200

    add deliverers cards output from strapi data

--- src/pages/deliverers.js
+++ src/pages/deliverers.js
@@ -1,22 +1,27 @@
 import Filter from "@/components/Filter/Filter";
 import DelivererCard from "@/components/DelivererCard/DelivererCard";
 
+
 const deliverers = (props) => {
-  console.log(props)
+  
+  const deliverersData = props.data.data;
+  console.log(deliverersData)
   return (
     <>
         <Filter/>
-        <DelivererCard/>
+        {deliverersData.map(deliverer => {
+          return <DelivererCard {...deliverer.attributes} thumbnail={deliverer.attributes.thumbnail.data[0].attributes.url}/>
+        })}
+        
     </>
   )
 }
-
-async function getStaticProps(context) {
-  const res = await fetch(`http://localhost:1337/api/deliverers-cards?populate=*`)
-  const data = await res.json()
-  return {
-      props: {data}, 
-  }
-}
-
 export default deliverers
+
+export async function getStaticProps(context) {
+    const res = await fetch(`http://localhost:1337/api/deliverers-cards?populate=*`)
+    const data = await res.json()
+    return {
+        props: {data}, 
+    }
+}
