2fddbd3d2850c1cdfdafa616a92fd0883145a524
Vlad Khomenko <vladkhomenko@MacBook-Air-Vlad.local>
2023-03-15 20:23:18 +0200

    remove aos from hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,17 +1,11 @@
 import Link from 'next/link'
 import HeroImg from './HeroImg'
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-import { useEffect } from 'react'
 
 const Hero = () => {
-  useEffect(() => {
-    AOS.init()
-  }, [])
   return (
     <div className='hero'>
       <div className='container hero__container'>
-        <div data-aos='fade-left' className='hero__descr'>
+        <div className='hero__descr'>
           <div className='hero__descr-title'>Smart business credit cards</div>
           <div className='hero__descr-text'>
             Untitled is a next-gen financial technology company in the process
@@ -19,7 +13,7 @@ const Hero = () => {
           </div>
           <button>Sign Up</button>
         </div>
-        <div data-aos='fade-right' className='hero__image'>
+        <div className='hero__image'>
           <HeroImg></HeroImg>
         </div>
       </div>
