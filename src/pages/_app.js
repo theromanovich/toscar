fdf9e18c8b7ea76bae3a236e6538025b8b849105
Vlad Khomenko <theromanovich03@gmail.com>
2023-04-02 17:24:03 +0300

    add favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,23 +1,29 @@
-import Head from "next/head";
+import Head from 'next/head'
 
-import Layout from '../components/Layout/Layout';
+import Layout from '../components/Layout/Layout'
 
-import 'normalize.css/normalize.css';
+import 'normalize.css/normalize.css'
 import './../style/global.scss'
 
 const MyApp = ({ Component, pageProps }) => (
-    <Layout>
-        <Head>
-            <link href='https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600&display=swap' rel='stylesheet'/>
-            <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-            <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet"/>
+  <Layout>
+    <Head>
+      <link
+        href='https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600&display=swap'
+        rel='stylesheet'
+      />
+      <link rel='preconnect' href='https://fonts.googleapis.com' />
+      <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+      <link
+        href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap'
+        rel='stylesheet'
+      />
+      <link rel='icon' type='image/svg+xml' href='favicon/letter-favicon.ico' />
+    </Head>
+    <main>
+      <Component {...pageProps} />
+    </main>
+  </Layout>
+)
 
-        </Head>
-        <main>
-            <Component {...pageProps} />
-        </main>
-    </Layout>
-);
-
-export default MyApp;
+export default MyApp
