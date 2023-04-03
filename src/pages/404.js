36bfba698e36f34c136acf79d5a80539cbb51f93
Vlad Khomenko <theromanovich03@gmail.com>
2023-04-03 12:49:15 +0300

    add error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,6 @@
 const Error = () => (
-    <div>
-        <h1>
-            404 page not found
-        </h1>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt doloribus et hic impedit, molestiae rem rerum voluptate. Asperiores, deleniti et, ex facilis fuga itaque mollitia odit, sapiente sed velit veritatis.
-        </p>
-    </div>
-);
-
-export default Error;
+  <>
+    <p className='page-not-found'>Page not found</p>
+  </>
+)
+export default Error
